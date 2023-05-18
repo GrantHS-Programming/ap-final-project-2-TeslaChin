c10713a18f2d3add7f12d29be1e7569b95a0ebdf
Tesla Chin <teslachin@gmail.com>
2023-05-18 09:51:18 -0700

    Made the ghosts follow and float

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -6,18 +6,23 @@ public class Ghost : MonoBehaviour
 {
     public GameObject ghost;
     static int lim = 0;
-    void Start()
+    public GameObject target;
+    private void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+        lim = Random.Range(15, 30);
     }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Timer.counter >= lim)
         {
             Instantiate(ghost, new Vector3(Random.Range(0,-8), 1.2f, Random.Range(0,-6)), new Quaternion(0, Random.Range(0f,1f), 0, 0));
-            lim += Random.Range(5, 10);
+            lim += Random.Range(5, 30);
         }
+        var step = Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+        transform.LookAt(target.transform);
     }
+
 }
